392065d8aac3034e2152a31d135775bd44135e14
Tal Lerner <tlerner2026@kehillahstudent.org>
2022-10-03 12:19:02 -0700

    rectanglular spiral

--- q_1_31.py
+++ q_1_31.py
@@ -1,4 +1,5 @@
 """
+Tal Lerner
 Python Programming in Context
 Try It Out
 Page 32
@@ -6,28 +7,14 @@ Page 32
 
 from turtle import *
 
-def draw_rectangular_spiral(max_side):
-    """Draw a rectangular spiral, starting in the center.
-    
-    `max_side` is the length of the longest side of the spiral in pixels"""
-
-    # Remember: When range takes 2 parameters, the first parameter is the start
-    # and the second parameter is the end. So this range will start at 1 and
-    # stop before it gets to max_side + 1 (i.e. it will stop at max_side)
-    for side_length in range(1, max_side + 1, 5):
-        forward(side_length)
-        right(90)
-
-#################################### 1.31 ######################################
-# Write a draw_spiral function to turn MORE than 90 degrees for each iteration
-
 def draw_spiral(max_side):
     """
-    ADD A DOC STRING HERE!
+    Draw a spiral. It will draw a line each time a different length
+    and then turn 100 and repeat.
     """
     for side_length in range(1, max_side + 1, 5):
         forward(side_length)
-        right(90)
+        right(100)
 
-# This code calls the function you will modify:
+# This code calls the function:
 draw_spiral(100)
